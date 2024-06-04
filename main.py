83a746c846f98b08e5033c78874c8c9c45924fa6
har1s0n <zhepshenin1337@gmail.com>
2024-06-04 12:21:34 +0300

    [upd] - добавлен файл зависимостей; - добавлено распараллеливание процедуры скачивания файлов; - рефакторинг
    [fix]
    - исправлена работа функции extract_file() в части обработки .Z-архивов

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import gzip
+import subprocess
 import zipfile
 import requests
 import argparse
 import datetime
 import os
 import re
+import concurrent.futures
 
 
-def get_collection_id():
-    """"
-    Функция для получения ID коллекции
-    """
+def get_collection_id(collection_name):
     response = requests.get("https://fcnd.ru/api/getFilter/")
     response.raise_for_status()
     data = response.json()
@@ -21,10 +19,7 @@ def get_collection_id():
     raise ValueError(f"Collection '{collection_name}' not found.")
 
 
-def get_files_list(collection_id):
-    """"
-    Функция для получения списка файлов за определенный период
-    """
+def get_files_list(collection_id, dt_begin, dt_end):
     url = "https://fcnd.ru/api/getData/"
     params = {
         "filter[time_begin]": dt_begin,
@@ -37,24 +32,25 @@ def get_files_list(collection_id):
 
 
 def filter_files(files_list):
-    """
-    Функция для фильтрации списка файлов по заданной маске
-    """
     filtered_files = []
+    file_patterns = [
+        r"\.\d{2}g\.Z$",
+        r"RN\.rnx",
+        lambda filename: filename.endswith(".zip") and not re.search(r"\.rnx", filename)
+    ]
+
     for file in files_list:
         file_name = file["pk_file_name"]
-        if re.search(r"\.\d{2}g\.Z$", file_name) or \
-                re.search(r"RN\.rnx", file_name) or \
-                (file_name.endswith(".zip") and not re.search(r"\.rnx", file_name)):
+        if any(re.search(pattern, file_name) if isinstance(pattern, str) else pattern(file_name) for pattern in
+               file_patterns):
             filtered_files.append(file)
 
     return filtered_files
 
 
-def download_file(time_begin, file_name, download_dir):
-    """"
-    Функция для скачивания файла
-    """
+def download_file(file, download_dir):
+    time_begin = file["pt_time_begin"]
+    file_name = file["pk_file_name"]
     url = f"https://fcnd.ru/api/getData/"
     params = {
         "datafile[time_begin]": time_begin,
@@ -70,9 +66,6 @@ def download_file(time_begin, file_name, download_dir):
 
 
 def extract_file(file_name, download_dir):
-    """
-    Функция для распаковки файла
-    """
     file_path = os.path.join(download_dir, file_name)
     if file_name.endswith(".zip"):
         with zipfile.ZipFile(file_path, 'r') as zip_file:
@@ -85,39 +78,30 @@ def extract_file(file_name, download_dir):
                         zip_file.extract(extracted_file, download_dir)
                         break
     elif file_name.endswith(".Z"):
-        with gzip.open(file_path, 'rb') as gz_file:
-            extracted_content = gz_file.read()
-            extracted_file_name = os.path.splitext(file_name)[0]
-            extracted_file_path = os.path.join(download_dir, extracted_file_name)
-            with open(extracted_file_path, 'wb') as extracted_file:
-                extracted_file.write(extracted_content)
+        subprocess.run(f"gunzip -f {file_path}", shell=True)
 
 
-def main():
+def main(dt_begin, dt_end, collection_name):
     download_dir = "./downloads"
 
     if not os.path.exists(download_dir):
         os.makedirs(download_dir)
 
     try:
-        collection_id = get_collection_id()
-        files_list = get_files_list(collection_id)
+        collection_id = get_collection_id(collection_name)
+        files_list = get_files_list(collection_id, dt_begin, dt_end)
         filtered_file_list = filter_files(files_list)
 
-        for file in filtered_file_list:
-            time_begin = file["pt_time_begin"]
-            file_name = file["pk_file_name"]
-            download_file(time_begin, file_name, download_dir)
-            extract_file(file_name, download_dir)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for file in filtered_file_list:
+                executor.submit(download_file, file, download_dir)
+                executor.submit(extract_file, file['pk_file_name'], download_dir)
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
 def validate_datetime(datetime_str):
-    """
-    Проверяет, соответствует ли введенная дата и время формату "DD-MM-YYYY hh:mm:ss".
-    """
     pattern = r"^\d{2}-\d{2}-\d{4}\s\d{2}:\d{2}:\d{2}$"
     if not re.match(pattern, datetime_str):
         raise argparse.ArgumentTypeError("Неверный формат даты и времени (требуемый формат: DD-MM-YYYY hh:mm:ss).")
@@ -142,8 +126,5 @@ if __name__ == "__main__":
                         help="the collection on the FSCD from which the files will be assembled")
 
     args = parser.parse_args()
-    dt_begin = args.dt_begin
-    dt_end = args.dt_end
-    collection_name = args.collection
 
-    main()
+    main(args.dt_begin, args.dt_end, args.collection)
